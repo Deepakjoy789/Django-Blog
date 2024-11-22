23f74db11b886d032cff50775c31ece1d05ea16f
Deepak Joy <113782188+Deepakjoy789@users.noreply.github.com>
2024-11-22 18:18:41 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-$le6wwgyh^^p#%m*w1@kx1jve4piy(ym(n=0%*$)c2i*ox4x4f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-blog-vjo1.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
